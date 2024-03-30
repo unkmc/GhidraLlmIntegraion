3750bc5f96ae995a119ef7ea08a42d289d1d826a
unkmc <whoreson@cheerful.com>
2024-03-30 13:32:59 -0400

    Add configuration example

---
+++ GhidraLlmIntegration/src/main/java/ghidrallmintegration/GhidraLlmIntegrationConfigurationExample.java
@@ -0,0 +1,7 @@
+package ghidrallmintegration;
+
+public class GhidraLlmIntegrationConfigurationExample {
+	// Fill these out
+	public String model = "gpt-4-turbo-preview";
+	public String apiKey = "sk-1234";
+}
